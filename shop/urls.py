3e860751c6651d81cdc9f4292eb75b96250fed15
TomQuez <tom.quez@live.fr>
2023-12-01 11:44:36 +0100

    ajout vue add-to-cart

--- shop/urls.py
+++ shop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from store.views import index, product_detail
+from store.views import index, product_detail,add_to_cart
 from django.conf.urls.static import static
 from shop import settings
 from accounts.views import signup, logout_user,login_user
@@ -25,6 +25,7 @@ urlpatterns = [
     path('',index,name='index'),
     path('admin/', admin.site.urls),
     path('product/<str:slug>',product_detail,name='product'),
+    path('product/<str:slug>/add-to-cart/',add_to_cart,name='add-to-cart'),
     path('signup',signup,name='signup'),
     path('logout',logout_user,name='logout'),
     path('login',login_user,name='login'),
