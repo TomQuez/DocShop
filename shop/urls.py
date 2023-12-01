8a3e1aff49bac4b11de86e43983f97628f923f42
TomQuez <tom.quez@live.fr>
2023-12-01 10:30:32 +0100

    ajout vue login_user

--- shop/urls.py
+++ shop/urls.py
@@ -19,13 +19,15 @@ from django.urls import path
 from store.views import index, product_detail
 from django.conf.urls.static import static
 from shop import settings
-from accounts.views import signup
+from accounts.views import signup, logout_user,login_user
 
 urlpatterns = [
     path('',index,name='index'),
     path('admin/', admin.site.urls),
     path('product/<str:slug>',product_detail,name='product'),
-    path('signup',signup,name='signup')
+    path('signup',signup,name='signup'),
+    path('logout',logout_user,name='logout'),
+    path('login',login_user,name='login'),
     
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
