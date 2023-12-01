268bab3ea253b965b9891983c0b01892c4bf4253
TomQuez <tom.quez@live.fr>
2023-12-01 11:04:25 +0100

    ajout model Order application store

--- store/models.py
+++ store/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
 
+
+
+from shop.settings import AUTH_USER_MODEL
 # Create your models here.
 """
 Product
@@ -23,3 +26,12 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse("product",kwargs={'slug':self.slug})
+    
+class Order(models.Model):
+    user=models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.IntegerField(default=1)
+    ordered=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.product.name} ({self.quantity})'
